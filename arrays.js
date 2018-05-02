f68f6eaf02cf6949cdce72767acd9ae8ce58bde0
Emmanuel Anderson <manu1526@gmail.com>
2018-05-02 09:21:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ addElementToBeginningOfArray();
 
 function destructivelyAddElementToBeginningOfArray(){
   var typesOfWurst=['Ahle Wurst','Thuringer','Naumberger',];
-  typesOfWurst=['foo '...typesOfWurst,];
+  typesOfWurst=['foo ',...typesOfWurst];
 }
 destructivelyAddElementToBeginningOfArray();
 
