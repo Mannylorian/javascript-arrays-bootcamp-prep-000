876fcb6b24ec5802eb3614dabb5e8528cc40bb12
Emmanuel Anderson <manu1526@gmail.com>
2018-05-02 09:27:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ var chocolateBars=['snickers','hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array){
   var citiesInThuringia=['Weimar','Jena','Erfurt'];
-  ['Gera',...citiesInThuringia];
+  
 }
 addElementToBeginningOfArray('array');  
 
@@ -15,7 +15,7 @@ addElementToBeginningOfArray('array');
 
 function destructivelyAddElementToBeginningOfArray(array){
   var typesOfWurst=['Ahle Wurst','Thuringer','Naumberger',];
-  citiesInThuringia.unshift('Weisswurst');
+  
 }
 destructivelyAddElementToBeginningOfArray('array');
 
