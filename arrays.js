aeb9ffa59d1ab39ad4f2e75d84eb127d8a0c3c23
Emmanuel Anderson <manu1526@gmail.com>
2018-05-02 09:29:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ var chocolateBars=['snickers','hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array){
   var citiesInThuringia=['Weimar','Jena','Erfurt'];
-  
+  citiesInThuringia.unshift('Gera')
 }
 addElementToBeginningOfArray('array');  
 
