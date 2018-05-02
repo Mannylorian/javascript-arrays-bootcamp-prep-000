bdd7eb7350ad2ebab0f21b88e03ec3401bd1a5f8
Emmanuel Anderson <manu1526@gmail.com>
2018-05-02 10:04:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,6 @@ var chocolateBars=['snickers','hundred grand', 'kitkat', 'skittles'];
 //var candyString='foo';
 
 function addElementToBeginningOfArray(chocolateBars, candyString){
-  return [chocolateBars...candyString];}
+  return [candyString ...chocolateBars];}
 
 
